3f65c6a82baf4e30e6c2ff0ac8c09c07770d01a8
Rill wan <119643378+Rillwan@users.noreply.github.com>
2024-01-28 18:47:12 +0530

    updated

--- src/calc.js
+++ src/calc.js
@@ -1,48 +1,54 @@
-var screen = document.getElementById('screen');
-
-var u = document.getElementById('undo');
+var screen = document.getElementById("screen");
+var u = document.getElementById("undo");
 
 function btnClick(value) {
-    screen.value += value;
+  screen.value += value;
 }
 
 function clearScreen() {
-    screen.value = "";
+  screen.value = "";
+  u.value = "";
 }
 
 function findResult() {
-    u.value = screen.value
-    var result = eval(screen.value)
+  try {
+    u.value = screen.value;
+    var result = eval(screen.value);
     screen.value = result;
+  } catch (error) {
+    console.log("error");
+    alert("Add Next Number")
+  }
 }
 
 function calculation(value) {
-    screen.value += value;
+  screen.value += value;
 }
 
 function percentage(value) {
-    u.value = screen.value + value
-    var strValue = screen.value.split(/(?=[+-])/)
-    function calc() {
-        var sum = 0;
-        result = strValue.map(str => {
-            return Number(str);
-        });
-        num1 = (result[0] * result[1]) / 100;
-        num2 = result[0];
-        sum = num1 + num2;
-        return sum
-    }
-    screen.value = calc();
+  u.value = screen.value + value;
+  var strValue = screen.value.split(/(?=[+-])/);
+  console.log(strValue);
+  function calc() {
+    var sum = 0;
+    result = strValue.map((str) => {
+      return Number(str);
+    });
+    num1 = (result[0] * result[1]) / 100;
+    num2 = result[0];
+    sum = num1 + num2;
+    return sum;
+  }
+  screen.value = calc();
 }
 
 function del(value) {
-    screen.value = screen.value.slice(0, -1)
+  screen.value = screen.value.slice(0, -1);
 }
 
 screen.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("btn-eql").click();
-    }
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("btn-eql").click();
+  }
 });
